1d06b0e787bc1aa7aa0549019666eb1739fa69ab
Oboth03 <joboboth601@gmail.com>
2026-06-08 09:11:14 +0300

    .

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,8 @@
-import heroImage from "../assets/hero.png"
 
 function Hero({ title, subtitle }) {
   return (
     
     <section id="home" className="py-5 px-6 flex flex-col items-center justify-center text-white bg-gradient-to-b from-gray-900 to-black px-6 pt-24">
-      <img src={heroImage} alt="Hero" className="w-64 h-64 rounded-full object-cover" />
       <div className="text-center  px-4 max-w-5xl mx-auto">
          <h1 className="text-3xl md:text-4xl font-bold mb-5">{ title }</h1>
         <p className="text-lg md:text-2xl leading-relaxed text-gray-300 mb-5">{subtitle}</p>
